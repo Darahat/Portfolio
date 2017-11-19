ff9231bd71980a1ca7ccc692ec1b76a2252e795c
darahat <darahat42@gmail.com>
2017-11-19 18:16:04 +0600

    5.2.0 no commit

--- js/section3.js
+++ js/section3.js
@@ -1,7 +1,7 @@
 $(function(){
 $('.panel').hover(function(){
-        $(this).find('.panel-footer').slideDown(250);
+        $(this).find('.panel-footer').slideDown(550);
     },function(){
-        $(this).find('.panel-footer').slideUp(250); //.fadeOut(205)
+        $(this).find('.panel-footer').slideUp(550); //.fadeOut(205)
     });
 })
